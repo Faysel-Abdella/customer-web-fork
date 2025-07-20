92511ce9f5ddc64701cf31da8467c8ba5447226e
Abubeker Abduljelil <abubekera87@gmail.com>
2025-07-20 23:57:03 +0300

    feat: add delivery time to the restaurant detail page

--- src/app/[locale]/(public)/restaurants/[restaurantId]/_components/RestaurantHeader.tsx
+++ src/app/[locale]/(public)/restaurants/[restaurantId]/_components/RestaurantHeader.tsx
@@ -8,18 +8,26 @@ const RestaurantHeader = ({ restaurant }: RestaurantHeaderProps) => {
   return (
     <>
       <div className="flex w-full items-center justify-between gap-5 py-6 pt-3 max-sm:flex-col">
-        <div>
+        <div className="flex">
           <p className="text-3xl font-semibold">{restaurant.title}</p>
         </div>
         <div className="flex items-center gap-7">
-          <div className="flex flex-col items-end">
+          <div className="flex flex-col items-center max-sm:gap-2 sm:items-end">
+            <p className="text-muted-foreground text-sm max-sm:text-center">
+              Earliest Time of arrival
+            </p>
+            <p className="font-medium">{restaurant.estimated_delivery_time}</p>
+          </div>
+          <div className="bg-border h-8 w-px" />
+
+          <div className="flex flex-col items-center max-sm:gap-2 sm:items-end">
             <p className="text-muted-foreground text-sm">Location</p>
             <p className="font-medium">{restaurant.location}</p>
           </div>
           <div className="bg-border h-8 w-px" />
-          <div className="flex flex-col items-end">
-            <p className="text-muted-foreground text-sm">Earliest arrival</p>
-            <p className="font-medium">{restaurant.estimated_delivery_time}</p>
+          <div className="flex flex-col items-center max-sm:gap-2 sm:items-end">
+            <p className="text-muted-foreground text-sm">Delivery fee</p>
+            <p className="font-medium">${restaurant.fee}</p>
           </div>
         </div>
       </div>
