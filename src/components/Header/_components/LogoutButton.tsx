40b81319ced9111c450fa5cd21d6f3821b97530a
Abubeker Abduljelil <abubekera87@gmail.com>
2025-07-28 22:32:10 +0300

    fix: fix auth state and logout redirection

--- src/components/Header/_components/LogoutButton.tsx
+++ src/components/Header/_components/LogoutButton.tsx
@@ -1,5 +1,6 @@
-import { PropsWithChildren } from "react";
+"use client";
 
+import { Loader2, LogOut } from "lucide-react";
 import { useTranslations } from "next-intl";
 
 import {
@@ -12,14 +13,36 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
 import { useAuth } from "@/contexts/AuthContext";
+import { cn } from "@/lib/utils";
 
-const LogoutButton = ({ children }: PropsWithChildren) => {
-  const { logout } = useAuth();
+interface LogoutButtonProps {
+  className?: string;
+}
+const LogoutButton = ({ className }: LogoutButtonProps) => {
+  const { logout, isLoading } = useAuth();
   const t = useTranslations("header");
   return (
     <AlertDialog>
-      <AlertDialogTrigger asChild>{children}</AlertDialogTrigger>
+      <AlertDialogTrigger asChild>
+        <Button
+          className={cn(
+            "hover:bg-secondary hover:text-secondary-foreground bg-background text-foreground w-full shadow-none",
+            className,
+          )}
+          disabled={isLoading}
+        >
+          {isLoading ? (
+            <Loader2 className="animate-spin" />
+          ) : (
+            <>
+              <LogOut className="mr-2 h-4 w-4" />
+            </>
+          )}
+          <span>{t("logout")}</span>
+        </Button>
+      </AlertDialogTrigger>
       <AlertDialogContent>
         <AlertDialogHeader>
           <AlertDialogTitle>{t("logout_warning")}</AlertDialogTitle>
